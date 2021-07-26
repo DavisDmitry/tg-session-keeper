8e2dcebfc11f6226eaef2c9270cb4af5f11d92dc
Dmitry <carlversalski@gmail.com>
2021-07-26 20:19:10 +0500

    ✅ Update tests for new ejs

--- tests/storage/test_ejs.py
+++ tests/storage/test_ejs.py
@@ -5,12 +5,12 @@ import pytest
 from telethon import TelegramClient
 
 from session_keeper.storage import (
-    EncryptedJsonStorage,
     InvalidPassword,
     MismatchedVersionError,
     StorageNotFound,
     StorageSettedError,
 )
+from session_keeper.storage.ejs import FileEncryptedJsonStorage as EncryptedJsonStorage
 
 PASSWORD = "qwerty"
 
@@ -20,7 +20,7 @@ pytestmark = pytest.mark.asyncio
 
 @pytest.fixture
 async def storage(temp_file: str, api_id: int, api_hash: str) -> EncryptedJsonStorage:
-    storage = EncryptedJsonStorage(PASSWORD, filename=temp_file)
+    storage = EncryptedJsonStorage(PASSWORD, temp_file)
     await storage.setup(api_id, api_hash)
     return storage
 
@@ -47,7 +47,7 @@ async def test_encrypt_and_decrypt(storage_with_session: EncryptedJsonStorage):
     storage1 = storage_with_session
     await storage1.save()
 
-    async with EncryptedJsonStorage(PASSWORD, filename=filename) as storage2:
+    async with EncryptedJsonStorage(PASSWORD, filename) as storage2:
         client = TelegramClient(
             storage2.sessions[0], storage2.api_id, storage2.api_hash
         )
@@ -74,12 +74,6 @@ async def test_storage_setted_error(
 ):
     with pytest.raises(StorageSettedError):
         await storage.setup(api_id, api_hash)
-
-
-async def test_mismatched_version_error(storage_file: str):
-    storage = EncryptedJsonStorage(PASSWORD, filename=storage_file, version=1)
-    with pytest.raises(MismatchedVersionError):
-        await storage.start()
 
 
 async def test_invalid_password(storage: EncryptedJsonStorage):

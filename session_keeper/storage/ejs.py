6b4fb0f86f9c6f371357586a6a9ded08027535bb
Dmitry <carlversalski@gmail.com>
2021-07-26 20:17:35 +0500

    🏗️ Split EncrytedJsonStorage to MemoryEnryptedJsonStorage and FileEncryptedJsonStorage

--- session_keeper/storage/ejs.py
+++ session_keeper/storage/ejs.py
@@ -1,7 +1,7 @@
 import base64 as b64
 import json
 import os
-from typing import Union
+from typing import Optional, Union
 
 from ..session import KeeperSession
 from . import exceptions as exc
@@ -12,18 +12,19 @@ PASS_HASH_SALT = b""
 PASS_HASH_ITERATES = 8
 
 
-class EncryptedJsonStorage(MemoryStorage):
+class MemoryEncryptedJsonStorage(MemoryStorage):
     def __init__(
-        self,
-        password: Union[bytes, str],
-        filename: str,
-        *,
-        version: Union[bytes, int] = CURRENT_VERSION,
+        self, password: Union[bytes, str], *, unencrytped_data: Optional[bytes] = None
     ):
         super().__init__()
-        self._filename = filename
-        self._version = bytes(version)
         self._set_fernet(password)
+        self._unencrypted_data = unencrytped_data
+        self._encrypted_data = None
+
+    @property
+    def encrypted_data(self) -> bytes:
+        self._save()
+        return self._encrypted_data
 
     @staticmethod
     def transform_password(
@@ -48,10 +49,6 @@ class EncryptedJsonStorage(MemoryStorage):
         if isinstance(password, str):
             password = self.transform_password(password, hash_salt, hash_iterates)
         self._fernet = Fernet(password)
-
-    @property
-    def filename(self) -> str:
-        return self._filename
 
     def _as_dict(self) -> dict:
         sessions = []
@@ -89,27 +86,49 @@ class EncryptedJsonStorage(MemoryStorage):
         data = b64.urlsafe_b64decode(data)
         await self._from_json(data.decode())
 
+    def _save(self) -> None:
+        self._encrypted_data = CURRENT_VERSION + self._encrypt_sessions()
+
     async def setup(self, api_id: int, api_hash: str) -> None:
         await super().setup(api_id, api_hash)
         await self.save()
 
-    async def start(self):
-        if not os.path.isfile(self.filename):
-            raise exc.StorageNotFound(f"File {self.filename} does not exist.")
-        with open(self.filename, "rb") as file:
-            data = file.read()
+    async def start(self) -> None:
+        data = self._unencrypted_data
+        if data:
             version, data = data[:1], data[1:]
-            if not data:
-                raise exc.StorageNotFound(f"File {self.filename} is empty.")
-            if version != self._version:
+            if version != CURRENT_VERSION:
                 raise exc.MismatchedVersionError(
                     "The version of the file does not match the version of the storage."
                 )
             await self._decrypt_sessions(data)
 
     async def save(self) -> None:
+        self._save()
+
+
+class FileEncryptedJsonStorage(MemoryEncryptedJsonStorage):
+    def __init__(self, password: Union[bytes, str], filename: str):
+        super().__init__(password)
+        self._filename = filename
+
+    @property
+    def filename(self) -> str:
+        return self._filename
+
+    async def start(self):
+        if not os.path.isfile(self.filename):
+            raise exc.StorageNotFound(f"File {self.filename} does not exist.")
+        with open(self.filename, "rb") as file:
+            data = self._unencrypted_data = file.read()
+            if not data:
+                raise exc.StorageNotFound(f"File {self.filename} is empty.")
+            await (super()).start()
+
+    async def save(self) -> None:
+        self._save()
         with open(self.filename, "wb") as file:
-            file.write(self._version + self._encrypt_sessions())
+            file.write(self.encrypted_data)
             file.flush()
 
     async def stop(self) -> None:

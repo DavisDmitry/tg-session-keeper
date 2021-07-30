9640363b9fd9ca0062c2c353cb6044047c013390
Dmitry <carlversalski@gmail.com>
2021-07-30 18:59:40 +0500

    ✅ Upgrade ejs tests

--- tests/storage/test_ejs.py
+++ tests/storage/test_ejs.py
@@ -1,6 +1,3 @@
-from tempfile import NamedTemporaryFile
-from typing import Iterator
-
 import pytest
 from telethon import TelegramClient
 
@@ -10,7 +7,10 @@ from session_keeper.storage import (
     StorageNotFound,
     StorageSettedError,
 )
-from session_keeper.storage.ejs import FileEncryptedJsonStorage as EncryptedJsonStorage
+from session_keeper.storage.ejs import (
+    FileEncryptedJsonStorage,
+    MemoryEncryptedJsonStorage,
+)
 
 PASSWORD = "qwerty"
 
@@ -19,64 +19,95 @@ pytestmark = pytest.mark.asyncio
 
 
 @pytest.fixture
-async def storage(temp_file: str, api_id: int, api_hash: str) -> EncryptedJsonStorage:
-    storage = EncryptedJsonStorage(PASSWORD, temp_file)
+async def memory_storage(api_id: int, api_hash: str) -> MemoryEncryptedJsonStorage:
+    storage = MemoryEncryptedJsonStorage(PASSWORD)
     await storage.setup(api_id, api_hash)
     return storage
 
 
 @pytest.fixture
-async def storage_with_session(
-    client_with_session: TelegramClient, storage: EncryptedJsonStorage
-) -> EncryptedJsonStorage:
-    await storage.add_session(client_with_session.session)
+async def file_storage(
+    temp_file: str, api_id: int, api_hash: str
+) -> FileEncryptedJsonStorage:
+    storage = FileEncryptedJsonStorage(PASSWORD, temp_file)
+    await storage.setup(api_id, api_hash)
     return storage
 
 
 @pytest.fixture
-def storage_file() -> Iterator[str]:
-    with NamedTemporaryFile("wb") as file:
-        file.write(b"5_")
-        file.flush()
-        yield file.name
+async def memory_storage_with_session(
+    client_with_session: TelegramClient, memory_storage: MemoryEncryptedJsonStorage
+) -> MemoryEncryptedJsonStorage:
+    await memory_storage.add_session(client_with_session.session)
+    return memory_storage
 
 
-async def test_encrypt_and_decrypt(storage_with_session: EncryptedJsonStorage):
-    filename = storage_with_session.filename
-
-    storage1 = storage_with_session
-    await storage1.save()
-
-    async with EncryptedJsonStorage(PASSWORD, filename) as storage2:
-        client = TelegramClient(
-            storage2.sessions[0], storage2.api_id, storage2.api_hash
-        )
-        async with client:
-            assert storage2.api_id == storage1.api_id
-            assert storage2.api_hash == storage1.api_hash
-            assert len(storage2.sessions) > 0
+@pytest.fixture
+async def file_storage_with_session(
+    client_with_session: TelegramClient, file_storage: FileEncryptedJsonStorage
+) -> FileEncryptedJsonStorage:
+    await file_storage.add_session(client_with_session.session)
+    return file_storage
 
 
-async def test_remove_session(storage_with_session: EncryptedJsonStorage):
-    storage = storage_with_session
-    await storage.remove_session(0)
-    assert len(storage.sessions) == 0
+class TestEncryptAndDecrypt:
+    async def test_memory(
+        self, memory_storage_with_session: MemoryEncryptedJsonStorage
+    ):
+        storage1 = memory_storage_with_session
+        async with MemoryEncryptedJsonStorage(
+            PASSWORD, encrypted_data=storage1.encrypted_data
+        ) as storage2:
+            assert storage1.api_id == storage2.api_id
+            assert storage1.api_hash == storage2.api_hash
+            client = TelegramClient(
+                storage2.sessions[0], storage2.api_id, storage2.api_hash
+            )
+            async with client:
+                assert storage1.sessions[0].as_dict() == storage2.sessions[0].as_dict()
+
+    async def test_file(self, file_storage_with_session: FileEncryptedJsonStorage):
+        storage1 = file_storage_with_session
+        await storage1.save()
+
+        async with FileEncryptedJsonStorage(PASSWORD, storage1.filename) as storage2:
+            assert storage1.api_id == storage2.api_id
+            assert storage1.api_hash == storage2.api_hash
+            client = TelegramClient(
+                storage2.sessions[0], storage2.api_id, storage2.api_hash
+            )
+            async with client:
+                assert storage1.sessions[0].as_dict() == storage2.sessions[0].as_dict()
 
 
-async def test_storage_not_found():
-    storage = EncryptedJsonStorage(PASSWORD, filename="qwerty")
-    with pytest.raises(StorageNotFound):
-        await storage.start()
+async def test_invalid_password(memory_storage: MemoryEncryptedJsonStorage):
+    storage2 = MemoryEncryptedJsonStorage(
+        "invalid password", encrypted_data=memory_storage.encrypted_data
+    )
+    with pytest.raises(InvalidPassword):
+        await storage2.start()
+
+
+class TestStorageNotFound:
+    async def test_file_doesnt_exist(self):
+        storage = FileEncryptedJsonStorage(PASSWORD, filename="qwerty")
+        with pytest.raises(StorageNotFound):
+            await storage.start()
+
+    async def test_file_without_data(self, temp_file: str):
+        storage = FileEncryptedJsonStorage(PASSWORD, temp_file)
+        with pytest.raises(StorageNotFound):
+            await storage.start()
 
 
 async def test_storage_setted_error(
-    storage: EncryptedJsonStorage, api_id: int, api_hash: str
+    file_storage: FileEncryptedJsonStorage, api_id: int, api_hash: str
 ):
     with pytest.raises(StorageSettedError):
-        await storage.setup(api_id, api_hash)
+        await file_storage.setup(api_id, api_hash)
 
 
-async def test_invalid_password(storage: EncryptedJsonStorage):
-    storage2 = EncryptedJsonStorage("invalid password", filename=storage.filename)
-    with pytest.raises(InvalidPassword):
-        await storage2.start()
+async def test_mismatched_version_error():
+    storage = MemoryEncryptedJsonStorage(PASSWORD, encrypted_data=b"5_fake_data")
+    with pytest.raises(MismatchedVersionError):
+        await storage.start()
